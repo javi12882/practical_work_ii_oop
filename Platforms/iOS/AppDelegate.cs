c2ad2075f1c3564f6a0ed31a5286da8780cdd938
javi12882 <javapro35@gmail.com>
2025-05-21 12:00:43 +0200

    q

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ConversorCalculator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
